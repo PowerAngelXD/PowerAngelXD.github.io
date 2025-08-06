3267af3159dd1eb5195a23570e4068d47b5d4130
PowerAngelXD <692732163@qq.com>
2025-08-06 10:26:47 +0800

    test update

--- src/config.ts
+++ src/config.ts
@@ -43,17 +43,7 @@ export const navBarConfig: NavBarConfig = {
 	links: [
 		LinkPreset.Home,
 		LinkPreset.Archive,
-		LinkPreset.About,
-		{
-			name: "GitHub",
-			url: "https://github.com/PowerAngelXD", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
-		{
-			name: "Bilibili",
-			url: "https://space.bilibili.com/399660558", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		}
+		LinkPreset.About
 	],
 };
 
